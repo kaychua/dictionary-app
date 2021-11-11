a720de399971c639806d95e6ef38a7c5de3f5c45
kaychua <kaychua.exe@gmail.com>
2021-11-11 19:32:48 +1100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Dictionoory App</h1>
+        <h2>Hey, I bet I can tell you what these words mean</h2>
+      </header>
+      <a href="#" className="btn btn-primary">
+        Hello
+      </a>
+    </div>
+  );
+}
