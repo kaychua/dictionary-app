fcce9187c2f80fb5a78e1e4151f70f124b729ba5
kaychua <kaychua.exe@gmail.com>
2021-11-12 12:42:17 +1100

    Definition and Meaning components rendered

--- src/DictionarySearchEngine.js
+++ src/DictionarySearchEngine.js
@@ -1,23 +1,25 @@
 import React, { useState } from "react";
 import axios from "axios";
+import Results from "./Results";
 
 export default function DictionarySearchEngine() {
-  const [define, setDefine] = useState("");
+  const [keyword, setKeyword] = useState("");
+  const [results, setResults] = useState(null);
 
   function handleResponse(response) {
     console.log(response.data[0]);
+    setResults(response.data[0]);
   }
 
   function search(event) {
     event.preventDefault();
 
-    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${define}`;
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
     axios.get(apiUrl).then(handleResponse);
   }
 
   function handleChange(event) {
-    event.preventDefault();
-    setDefine(event.target.value);
+    setKeyword(event.target.value);
   }
 
   return (
@@ -25,12 +27,13 @@ export default function DictionarySearchEngine() {
       <form onSubmit={search}>
         <input
           type="search"
-          autofocus="on"
+          autoFocus="on"
           placeholder="Search for a word"
           onChange={handleChange}
         />
         <button type="submit">🔎</button>
       </form>
+      <Results results={results} />
     </div>
   );
 }
