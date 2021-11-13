ec4af37541f67896189c54121633e7a1ea0ca958
kaychua <kaychua.exe@gmail.com>
2021-11-13 20:46:44 +1100

    synonyms updated
    <REMEMBER TO RETURN AFTER MAPPING>

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Synonym from "./Synonym";
 
 export default function Meaning(props) {
   return (
@@ -8,10 +9,13 @@ export default function Meaning(props) {
         return (
           <div key={index}>
             <p>
+              <strong>Definition: </strong>
               {definitions.definition}
               <br />
+              <strong>Example: </strong>
               <em>{definitions.example}</em>
             </p>
+            <Synonym synonyms={definitions.synonyms} />
           </div>
         );
       })}
